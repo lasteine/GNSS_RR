0f3bb903649af5fbac1b7906cb6041b5bb018fbf
lasteine <63645252+lasteine@users.noreply.github.com>
2023-06-22 10:21:02 +0200

    Variable added for station name

--- run_gnssrefl_ubuntu.sh
+++ run_gnssrefl_ubuntu.sh
@@ -3,15 +3,16 @@ year_start=$1
 doy_start=$2
 year_end=$3
 doy_end=$4
+station=$5 # nmlb, nmsh
 echo " "
 echo "++++++++++++++++++++++++"
 echo "  Convert rinex to SNR  "
 echo "++++++++++++++++++++++++"
 echo " "
-rinex2snr nmlb $year_start $doy_start -nolook=True -year_end $year_end -doy_end $doy_end -orb gnss -overwrite=True
+rinex2snr $station $year_start $doy_start -nolook=True -year_end $year_end -doy_end $doy_end -orb gnss -overwrite=True
 echo " "
 echo "++++++++++++++++++++++++"
 echo "    Do Reflectometry    "
 echo "++++++++++++++++++++++++"
 echo " "
-gnssir nmlb $year_start $doy_start -plt=False -year_end $year_end -doy_end $doy_end
+gnssir $station $year_start $doy_start -plt=False -year_end $year_end -doy_end $doy_end
